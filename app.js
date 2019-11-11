bcea4e7361886c63ec10e635d2c3e93bfe8d6672
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-11-12 00:45:26 +0530

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
@@ -11,16 +11,21 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser:true});
+
+const itemsSchema = new mongoose.Schema({
+  name : String,
+});
+
+const Item = mongoose.model("Item" , itemsSchema);
 
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today" , newListItems: items});
 
 });
 
