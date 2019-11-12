1e6a86e270256e9c5d3bf7770f88bc04313227a5
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-11-13 01:15:06 +0530

    commit

--- app.js
+++ app.js
@@ -67,15 +67,13 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name : itemName
+  });
+item.save();
+res.redirect("/");
+  
 });
 
 app.get("/work", function(req,res){
