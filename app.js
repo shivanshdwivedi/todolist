c5fcd870d5f65b13c43c801f7c4fe6e434070ba0
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-11-14 03:08:20 +0530

    commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-shivansh:shiv**2406@cluster0-zpipx.mongodb.net/todolistDB", {useNewUrlParser:true});
 
 const itemsSchema = new mongoose.Schema({
   name : String
